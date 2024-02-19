af1dc194c04322cbde84780ad5581432a9698842
Sam Salek <33421876+sam-salek@users.noreply.github.com>
2024-02-19 13:44:58 +0100

    Selector script now exports test classes in seperate txt files for each project

--- scripts/junit_test_selector.py
+++ scripts/junit_test_selector.py
@@ -7,7 +7,7 @@ project2_tests = r"scripts\output\RxJava-3.1.8.txt"                 # ALL RxJava
 project3_tests = r"scripts\output\stubby4j-7.6.0.txt"               # ALL stubby4j tests
 
 num_tests = 30  # Number of junit tests to select from each project
-
+jmh_command_base = "java -jar \"ju2jmh-jmh.jar\" -f 1 -wi 0 -i 1 -r 100ms -foe true"
 def select_random_junit_tests(project_tests_path, num_tests):
     with open(project_tests_path, "r") as f:
         lines = f.readlines()
@@ -26,6 +26,7 @@ def select_random_junit_tests(project_tests_path, num_tests):
 
 def generate_output(project1_tests, project2_tests, project3_tests):
     print("Selecting junit tests...")
+    selected_tests = {}
 
     current_time = datetime.now().time()
     time_str = str(current_time.hour) + "h" + \
@@ -37,20 +38,37 @@ def generate_output(project1_tests, project2_tests, project3_tests):
         for project_tests in [project1_tests, project2_tests, project3_tests]:
             
             project_tests_path, num_tests = project_tests
-            selected_tests = select_random_junit_tests(project_tests_path, num_tests)
-
             project_name = extract_project_name(project_tests_path)
+            selected_tests[project_name] = select_random_junit_tests(project_tests_path, num_tests)
 
+            jmh_command = jmh_command_base
             f.write(f"# {project_name} | SELECTED {num_tests} TESTS \n ROW IN ORIGIN FILE | TEST NR. | TEST METHOD PATH \n")
             i = 1
-            for row_number, row_content in selected_tests:
+            for row_number, row_content in selected_tests[project_name]:
                 row_number_str = str(row_number).rjust(7)
                 f.write(f"{row_number_str} |     {i}.    {row_content[:7]}{row_content[7:]}\n")
                 i += 1
+                jmh_command += ' ' + row_content
             f.write("\n")
+
+            f.write(jmh_command + '\n\n')
 
             print(f"{project_name} done!")
     print("ALL DONE!")
+
+    for project_tests in [project1_tests, project2_tests, project3_tests]:
+        
+        project_tests_path, num_tests = project_tests
+        project_name = extract_project_name(project_tests_path)
+
+        with open(os.path.join(os.path.dirname(__file__), "output", f"{project_name}-{time_str}.txt"), "w") as f:
+            class_names = []
+            for row_number, row_content in selected_tests[project_name]:
+                row_content = row_content[:row_content.rfind('.')]
+                class_names.append(row_content)
+            for class_name in list(dict.fromkeys(class_names)):
+                f.write(f"{class_name}\n")
+            print(f"{project_name} done again!")
 
 def extract_project_name(file_path):
     start_index = file_path.find("scripts\\output\\") + len("scripts\\output\\")
