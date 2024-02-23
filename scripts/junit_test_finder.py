02536395a468b2606e5198dbb68787d93985b88d
Sam Salek <33421876+sam-salek@users.noreply.github.com>
2024-02-23 11:55:35 +0100

    Updated finder script to handle multiple test directories for each project

--- scripts/junit_test_finder.py
+++ scripts/junit_test_finder.py
@@ -21,20 +21,22 @@ def find_junit_tests(java_file_path):
         
     return junit_tests
 
-# Finds all junit tests within the given directory
-def find_all_junit_tests(directory):
+# Finds all junit tests within the given directories
+def find_all_junit_tests(directories):
     junit_tests = []
-    java_files = find_java_files(directory)
-    for file in java_files:
-        test_methods = find_junit_tests(file)
-        for test in test_methods:
-            file_no_extension = file[:-5]
-            junit_tests.append(file_no_extension + "\\" + test)
+    
+    for d in directories:
+        java_files = find_java_files(d)
+        for file in java_files:
+            test_methods = find_junit_tests(file)
+            for test in test_methods:
+                file_no_extension = file[:-5]
+                junit_tests.append(file_no_extension + "\\" + test)
     return junit_tests
 
-# Creates a txt file containing all 'junit_tests' within the directory of 'test_directory'
-def generate_txt(junit_tests, test_directory):
-    file_name = extract_project_name(test_directory) + "_ALL" + ".txt"
+# Creates a txt file containing all 'junit_tests' and their paths
+def generate_txt(junit_tests, project_name):
+    file_name =  project_name + "_ALL" + ".txt"
     txt_path = os.path.join(os.path.dirname(__file__), "output", file_name)
     if not os.path.exists(os.path.dirname(txt_path)):
         os.makedirs(os.path.dirname(txt_path))
@@ -55,37 +57,31 @@ def process_path(path):
     
     return path
 
-# Extracts project name from all projects in the 'projects' folder
-def extract_project_name(file_path):
-    start_index = file_path.find("projects\\") + len("projects\\")
-    end_index = file_path.find("\\", start_index)
-    if end_index != -1:
-        return file_path[start_index:end_index]
-    else:
-        return file_path[start_index:]
+# Exports all found junit tests within 'test_directories'
+def export_junit_tests(test_directories, project_name):
+    junit_tests = find_all_junit_tests(test_directories)
+    generate_txt(junit_tests, project_name)
 
-# Exports all found junit tests within 'test_directory'
-def export_junit_tests(test_directory):
-    junit_tests = find_all_junit_tests(test_directory)
-    generate_txt(junit_tests, test_directory)
-
-path_1 = r"benchmark-heuristics\projects\mockito-5.10.0\src\test\java\org"                                                   # Mockito
-path_2 = r"benchmark-heuristics\projects\RxJava-3.1.8\src\test\java\io\reactivex\rxjava3"                                    # RxJava
-path_3 = r"benchmark-heuristics\projects\stubby4j-7.6.0\src\test\java\io\github\azagniotov\stubby4j"                         # stubby4j
+path_1 = (r"projects\mockito-5.10.0\src\test\java\org",)                               # Mockito
+path_2 = (r"projects\RxJava-3.1.8\src\test\java\io\reactivex\rxjava3",)                # RxJava
+path_3 = (r"projects\stubby4j-7.6.0\src\functional-test",                              # stubby4j
+          r"projects\stubby4j-7.6.0\src\integration-test",
+          r"projects\stubby4j-7.6.0\src\load-test",
+          r"projects\stubby4j-7.6.0\src\test")                                             
 
 # Mockito
 print("Exporting Mockito junit tests...", end="")
-export_junit_tests(path_1)  
+export_junit_tests(path_1, "mockito-5.10.0")  
 print("done!")
 
 # RxJava
 print("Exporting RxJava junit tests...", end="")
-export_junit_tests(path_2)  
+export_junit_tests(path_2, "RxJava-3.1.8")  
 print("done!")
 
 # stubby4j
 print("Exporting stubby4j junit tests...", end="")
-export_junit_tests(path_3)  
+export_junit_tests(path_3, "stubby4j-7.6.0")  
 print("done!")
 
 print("ALL DONE!")
