b32ece9b3201645654cbebc0a4563fd098eb3bda
Sam Salek <33421876+sam-salek@users.noreply.github.com>
2024-03-11 12:17:27 +0100

    Added new print method that nicely prints the parsing.

--- projects/RxJava-3.1.8/src/main/java/parser/Main.java
+++ projects/RxJava-3.1.8/src/main/java/parser/Main.java
@@ -6,10 +6,35 @@ public class Main {
         String filePath = "E:\\Chalmers\\DATX05-MastersThesis\\benchmark-heuristics\\projects\\RxJava-3.1.8\\src\\test\\java\\io\\reactivex\\rxjava3\\internal\\schedulers\\InstantPeriodicTaskTest.java";
         String methodName = "dispose3";
 
+        // String filePath = "E:\\Chalmers\\DATX05-MastersThesis\\benchmark-heuristics\\projects\\RxJava-3.1.8\\src\\test\\java\\io\\reactivex\\rxjava3\\disposables\\DisposableTest.java";
+        // String methodName = "fromAction";
+
+        // String filePath = "E:\\Chalmers\\DATX05-MastersThesis\\benchmark-heuristics\\projects\\RxJava-3.1.8\\src\\test\\java\\io\\reactivex\\rxjava3\\core\\Retry.java";
+        // String methodName = "evaluate";
+
         MethodParser parser = new MethodParser(filePath, methodName);
         parser.run();
-        System.out.println(parser.getMethodCalls());
-        System.out.println(parser.getObjectInstantiations());
-        System.out.println(parser.getPackageAccesses());
+        print(parser);
+    }
+
+    /**
+     *
+     * @param parser
+     */
+    public static void print(MethodParser parser) {
+        System.out.println("\nMETHOD CALLS");
+        parser.getMethodCalls().forEach((k ,v) -> System.out.println(k + ": " + v));
+
+        System.out.println("\nOBJECT INSTANTIATIONS");
+        parser.getObjectInstantiations().forEach((k ,v) -> System.out.println(k + ": " + v));
+
+        System.out.println("\nPACKAGE ACCESSES");
+        parser.getPackageAccesses().forEach((k ,v) -> System.out.println(k + ": " + v));
+
+        System.out.println("\nSTATS");
+        System.out.println("Conditionals: " + parser.getConditionals());
+        System.out.println("Loops: " + parser.getLoops());
+        System.out.println("Nested loops: " + parser.getNestedLoops());
+        System.out.println("Method calls: " + parser.getMethodCallsAmount());
     }
 }
