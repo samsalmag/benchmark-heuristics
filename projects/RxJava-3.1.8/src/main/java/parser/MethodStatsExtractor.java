640d91abc1c7b1c18a8a831542eb91e9a19a7b09
Sam Salek <33421876+sam-salek@users.noreply.github.com>
2024-03-11 10:34:11 +0100

    Added more comments to MethodStatsExtractor.

--- projects/RxJava-3.1.8/src/main/java/parser/MethodStatsExtractor.java
+++ projects/RxJava-3.1.8/src/main/java/parser/MethodStatsExtractor.java
@@ -6,15 +6,31 @@ import com.github.javaparser.ast.Node;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-// Class that takes in a method of type MethodDeclaration,
-// and returns a hashmap of e.g. number of loops, nr of conditionals
-// Example usage:
-// HashMap<String, Integer> stats = ExtractMethodStats.getStats(method); // get statistics about method
-// System.out.println(stats);
-public class ExtractMethodStats {
-    public static HashMap<String, Integer> getStats(MethodDeclaration method) {
-        HashMap<String, Integer> methodStats = new HashMap<>();
+/**
+ * Class that takes in a method of type MethodDeclaration and returns a Map<String, Integer> of method stats.
+ * E.g. stats: number of loops, number of conditionals, etc.
+ *
+ * Example usage:
+ * Map<String, Integer> stats = MethodStatsExtractor.getStats(method);
+ * System.out.println(stats);
+ *
+ * @author Malte Åkvist
+ */
+public class MethodStatsExtractor {
+
+    // Private constructor. Use methods directly since they static.
+    private MethodStatsExtractor(){}
+
+    /**
+     * Get method stats from the provided method.
+     *
+     * @param method The method to extract stats from.
+     * @return A map containing all extracted method stats, with the map key corresponding to a type of stat.
+     */
+    public static Map<String, Integer> getStats(MethodDeclaration method) {
+        Map<String, Integer> methodStats = new HashMap<>();
 
         methodStats.put("Loops", countLoops(method));
         methodStats.put("Nested loops", countNestedLoops(method));
@@ -22,6 +38,7 @@ public class ExtractMethodStats {
         return methodStats;
     }
 
+    // Counts conditionals in a method
     private static int countConditionals(MethodDeclaration startMethod) {
         int ifCount = startMethod.findAll(IfStmt.class).size(); // Count if and else if statements
 
@@ -35,16 +52,19 @@ public class ExtractMethodStats {
         return ifCount + switchCaseCount;
     }
 
+    // Counts loops in a method
     private static int countLoops(MethodDeclaration startMethod) {
         List<Node> loops = startMethod.findAll(Node.class, n ->
                 n instanceof ForStmt || n instanceof WhileStmt || n instanceof DoStmt);
         return loops.size();
     }
 
+    // Counts nested loops in a method
     private static int countNestedLoops(MethodDeclaration startMethod) {
         return calculateDepth(startMethod, 0) - 1;
     }
 
+    // Counts depth of nested loops in a method
     private static int calculateDepth(Node node, int depth) {
         int maxDepth = depth;
         for (Node child : node.getChildNodes()) {
