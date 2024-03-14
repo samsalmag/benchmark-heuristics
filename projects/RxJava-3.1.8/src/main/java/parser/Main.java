5bb6bfce3b1bcd8db7847dde43f6b8c572316dd5
Sam Salek <33421876+sam-salek@users.noreply.github.com>
2024-03-14 16:09:03 +0100

    Changed main method to use new way of calling MethodParser.

--- projects/RxJava-3.1.8/src/main/java/parser/Main.java
+++ projects/RxJava-3.1.8/src/main/java/parser/Main.java
@@ -3,47 +3,60 @@ package parser;
 public class Main {
 
     public static void main(String[] args) {
-        String filePath1 = "E:\\Chalmers\\DATX05-MastersThesis\\benchmark-heuristics\\projects\\RxJava-3.1.8\\src\\test\\java\\io\\reactivex\\rxjava3\\internal\\schedulers\\InstantPeriodicTaskTest.java";
+        MethodParser parser = new MethodParser(Integer.MAX_VALUE,
+                                    "projects\\RxJava-3.1.8\\src\\main\\java\\",
+                                     "projects\\RxJava-3.1.8\\src\\main\\java\\",
+                                      "rxjava");
+
+        String filePath1 = "projects\\RxJava-3.1.8\\src\\test\\java\\io\\reactivex\\rxjava3\\internal\\schedulers\\InstantPeriodicTaskTest.java";
         String methodName1 = "dispose3";
-        MethodParser parser1 = new MethodParser(filePath1, methodName1);
-        parser1.run();
+        parser.parse(filePath1, methodName1);
 
-        String filePath2 = "E:\\Chalmers\\DATX05-MastersThesis\\benchmark-heuristics\\projects\\RxJava-3.1.8\\src\\test\\java\\io\\reactivex\\rxjava3\\disposables\\DisposableTest.java";
+        String filePath2 = "projects\\RxJava-3.1.8\\src\\test\\java\\io\\reactivex\\rxjava3\\disposables\\DisposableTest.java";
         String methodName2 = "fromAction";
-        MethodParser parser2 = new MethodParser(filePath2, methodName2);
-        parser2.run();
+        parser.parse(filePath2, methodName2);
 
-        String filePath3 = "E:\\Chalmers\\DATX05-MastersThesis\\benchmark-heuristics\\projects\\RxJava-3.1.8\\src\\test\\java\\io\\reactivex\\rxjava3\\core\\Retry.java";
+        String filePath3 = "projects\\RxJava-3.1.8\\src\\test\\java\\io\\reactivex\\rxjava3\\core\\Retry.java";
         String methodName3 = "evaluate";
+        parser.parse(filePath3, methodName3);
+
+        /* Unsolved exception
+        String filePath3 = "E:\\Chalmers\\DATX05-MastersThesis\\benchmark-heuristics\\projects\\RxJava-3.1.8\\src\\test\\java\\io\\reactivex\\rxjava3\\internal\\jdk8\\ParallelMapTryOptionalTest.java";
+        String methodName3 = "mapFailWithSkip";
         MethodParser parser3 = new MethodParser(filePath3, methodName3);
         parser3.run();
+        */
 
         JsonCreator jsonCreator = new JsonCreator("E:\\Chalmers\\DATX05-MastersThesis\\test.json");
-        jsonCreator.add(parser1.toParsedMethod(),
-                        parser2.toParsedMethod(),
-                        parser3.toParsedMethod());
-        jsonCreator.createJson();
+        //jsonCreator.add(parser1.toParsedMethod(),
+        //                parser2.toParsedMethod(),
+        //                parser3.toParsedMethod());
+        //jsonCreator.createJson();
+
+
+        print(parser.parse(filePath1, methodName1));
     }
 
     /**
-     * Nicely prints the parsing of the provided MethodParser.
+     * Nicely prints the parsing of the provided ParsedMethod.
      *
-     * @param parser The parser to print.
+     * @param parsedMethod The parsedMethod to print.
      */
-    public static void print(MethodParser parser) {
+    public static void print(ParsedMethod parsedMethod) {
         System.out.println("\nMETHOD CALLS");
-        parser.getMethodCalls().forEach((k ,v) -> System.out.println(k + ": " + v));
+        parsedMethod.getMethodCalls().forEach((k ,v) -> System.out.println(k + ": " + v));
 
         System.out.println("\nOBJECT INSTANTIATIONS");
-        parser.getObjectInstantiations().forEach((k ,v) -> System.out.println(k + ": " + v));
+        parsedMethod.getObjectInstantiations().forEach((k ,v) -> System.out.println(k + ": " + v));
 
         System.out.println("\nPACKAGE ACCESSES");
-        parser.getPackageAccesses().forEach((k ,v) -> System.out.println(k + ": " + v));
+        parsedMethod.getPackageAccesses().forEach((k ,v) -> System.out.println(k + ": " + v));
 
         System.out.println("\nSTATS");
-        System.out.println("Conditionals: " + parser.getNumConditionals());
-        System.out.println("Loops: " + parser.getNumLoops());
-        System.out.println("Nested loops: " + parser.getNumNestedLoops());
-        System.out.println("Method calls: " + parser.getNumMethodCalls());
+        System.out.println("Conditionals: " + parsedMethod.getNumConditionals());
+        System.out.println("Loops: " + parsedMethod.getNumLoops());
+        System.out.println("Nested loops: " + parsedMethod.getNumNestedLoops());
+        System.out.println("Method calls: " + parsedMethod.getNumMethodCalls());
+        System.out.println("Lines of code: " + parsedMethod.getLinesOfCode());
     }
 }
