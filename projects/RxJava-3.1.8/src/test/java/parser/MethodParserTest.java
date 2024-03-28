f44fcfe4375cd7daf993dc496bf67db54d890477
Sam Salek <33421876+sam-salek@users.noreply.github.com>
2024-03-28 01:40:38 +0100

    Moved some tests. Added many many many new tests.

--- projects/RxJava-3.1.8/src/test/java/parser/MethodParserTest.java
+++ projects/RxJava-3.1.8/src/test/java/parser/MethodParserTest.java
@@ -4,8 +4,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,45 +14,38 @@ public class MethodParserTest {
     @BeforeClass
     public static void init() {
         PARSER = new MethodParser(Integer.MAX_VALUE, "src\\main\\java\\", "src\\test\\java\\", "rxjava",
-                                    new File("src\\main\\java"),
-                                    new File("src\\test\\java"));
+                new File("src\\main\\java"),
+                new File("src\\test\\java"));
     }
 
     @Test
-    public void testPackageAccesses1() {
-        Map<String, Integer> expectedValues = new HashMap<>();
-        expectedValues.put("io.reactivex.rxjava3.plugins", 27);
-        expectedValues.put("java.lang", 31);
-        expectedValues.put("java.util.concurrent",10);
-        expectedValues.put("java.util.concurrent.atomic", 6);
-        expectedValues.put("io.reactivex.rxjava3.internal.schedulers", 4);
-        expectedValues.put("io.reactivex.rxjava3.exceptions", 1);
-        expectedValues.put("org.junit", 2);
-
-        String filePath = new File("src\\test\\java\\io\\reactivex\\rxjava3\\internal\\schedulers\\InstantPeriodicTaskTest.java").getAbsolutePath();
-        String methodName = "dispose3";
-        PARSER.parse(filePath, methodName);
-        Map<String, Integer> actualValues = PARSER.getParsedMethod().getPackageAccesses();
-
-        assertEquals(expectedValues, actualValues);
-    }
-
-    @Test
-    public void testMethodStatsConditional1() {
-        String filePath = new File("src\\test\\java\\io\\reactivex\\rxjava3\\internal\\schedulers\\InstantPeriodicTaskTest.java").getAbsolutePath();
-        String methodName = "dispose3";
+    public void numLoopsTest_shouldBeZero() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "empty";
         PARSER.parse(filePath, methodName);
 
-        int expectedValue = 32;
-        int actualValue = PARSER.getParsedMethod().getNumConditionals();
+        int expectedValue = 0;
+        int actualValue = PARSER.getParsedMethod().getNumLoops();
 
         assertEquals(expectedValue, actualValue);
     }
 
     @Test
-    public void testMethodStatsLoops1() {
-        String filePath = new File("src\\test\\java\\io\\reactivex\\rxjava3\\internal\\schedulers\\InstantPeriodicTaskTest.java").getAbsolutePath();
-        String methodName = "dispose3";
+    public void numLoopsTest_shouldBeOne() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "oneLoop";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 1;
+        int actualValue = PARSER.getParsedMethod().getNumLoops();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numLoopsTest_shouldBeTwo_1() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "twoLoop";
         PARSER.parse(filePath, methodName);
 
         int expectedValue = 2;
@@ -64,37 +55,325 @@ public class MethodParserTest {
     }
 
     @Test
-    public void testMethodStatsNestedLoops1() {
-        String filePath = new File("src\\test\\java\\io\\reactivex\\rxjava3\\internal\\schedulers\\InstantPeriodicTaskTest.java").getAbsolutePath();
-        String methodName = "dispose3";
+    public void numLoopsTest_shouldBeTwo_2() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "twoLoop_oneNestedLoop";
         PARSER.parse(filePath, methodName);
 
-        int expectedValue = 0;
+        int expectedValue = 2;
+        int actualValue = PARSER.getParsedMethod().getNumLoops();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numLoopsTest_shouldBeThree_1() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "threeLoop_oneNestedLoop";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 3;
+        int actualValue = PARSER.getParsedMethod().getNumLoops();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numLoopsTest_shouldBeThree_2() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "threeLoop_twoNestedLoop";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 3;
+        int actualValue = PARSER.getParsedMethod().getNumLoops();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numLoopsTest_shouldBeFour_1() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "fourLoop_oneNestedLoop";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 4;
+        int actualValue = PARSER.getParsedMethod().getNumLoops();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numLoopsTest_shouldBeFour_2() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "fourLoop_threeNestedLoop";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 4;
+        int actualValue = PARSER.getParsedMethod().getNumLoops();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numNestedLoopsTest_shouldBeOne_1() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "twoLoop_oneNestedLoop";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 1;
         int actualValue = PARSER.getParsedMethod().getNumNestedLoops();
 
         assertEquals(expectedValue, actualValue);
     }
 
     @Test
-    public void testMethodStatsMethodCalls1() {
-        String filePath = new File("src\\test\\java\\io\\reactivex\\rxjava3\\internal\\schedulers\\InstantPeriodicTaskTest.java").getAbsolutePath();
-        String methodName = "dispose3";
+    public void numNestedLoopsTest_shouldBeOne_2() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "threeLoop_oneNestedLoop";
         PARSER.parse(filePath, methodName);
 
-        int expectedValue = 0;
+        int expectedValue = 1;
+        int actualValue = PARSER.getParsedMethod().getNumNestedLoops();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numNestedLoopsTest_shouldBeOne_3() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "fourLoop_oneNestedLoop";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 1;
+        int actualValue = PARSER.getParsedMethod().getNumNestedLoops();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numNestedLoopsTest_shouldBeTwo() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "threeLoop_twoNestedLoop";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 2;
+        int actualValue = PARSER.getParsedMethod().getNumNestedLoops();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numNestedLoopsTest_shouldBeThree() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "fourLoop_threeNestedLoop";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 3;
+        int actualValue = PARSER.getParsedMethod().getNumNestedLoops();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numMethodCallsTest_shouldBeOne() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "oneMethodCall";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 1;
         int actualValue = PARSER.getParsedMethod().getNumMethodCalls();
 
         assertEquals(expectedValue, actualValue);
     }
 
     @Test
-    public void testMethodStatsLoc1() {
-        String filePath = new File("src\\test\\java\\io\\reactivex\\rxjava3\\internal\\schedulers\\InstantPeriodicTaskTest.java").getAbsolutePath();
-        String methodName = "dispose3";
+    public void numMethodCallsTest_shouldBeTwo() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "twoMethodCall";
         PARSER.parse(filePath, methodName);
 
-        int expectedValue = 141;
-        int actualValue = PARSER.getParsedMethod().getLinesOfCode();
+        int expectedValue = 2;
+        int actualValue = PARSER.getParsedMethod().getNumMethodCalls();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numMethodCallsTest_shouldBeThree() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "threeMethodCall";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 3;
+        int actualValue = PARSER.getParsedMethod().getNumMethodCalls();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numMethodCallsTest_shouldBeFour() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "fourMethodCall";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 4;
+        int actualValue = PARSER.getParsedMethod().getNumMethodCalls();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numMethodCallsTest_shouldBeEight() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "eightMethodCall";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 8;
+        int actualValue = PARSER.getParsedMethod().getNumMethodCalls();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numMethodCallsTest_shouldBeNine() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "nineMethodCall";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 9;
+        int actualValue = PARSER.getParsedMethod().getNumMethodCalls();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numMethodCallsTest_shouldBeThirty() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "thirtyMethodCall";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 30;
+        int actualValue = PARSER.getParsedMethod().getNumMethodCalls();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numConditionalsTest_shouldBeOne_1() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "oneIf_1";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 1;
+        int actualValue = PARSER.getParsedMethod().getNumConditionals();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numConditionalsTest_shouldBeOne_2() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "oneIf_2";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 1;
+        int actualValue = PARSER.getParsedMethod().getNumConditionals();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numConditionalsTest_shouldBeOne_3() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "oneSwitchCase";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 1;
+        int actualValue = PARSER.getParsedMethod().getNumConditionals();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numConditionalsTest_shouldBeTwo_1() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "twoIf_1";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 2;
+        int actualValue = PARSER.getParsedMethod().getNumConditionals();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numConditionalsTest_shouldBeTwo_2() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "twoIf_2";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 2;
+        int actualValue = PARSER.getParsedMethod().getNumConditionals();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numConditionalsTest_shouldBeTwo_3() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "twoIf_3";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 2;
+        int actualValue = PARSER.getParsedMethod().getNumConditionals();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numConditionalsTest_shouldBeTwo_4() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "twoSwitchCase";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 2;
+        int actualValue = PARSER.getParsedMethod().getNumConditionals();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numConditionalsTest_shouldBeThree_1() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "threeIf_1";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 3;
+        int actualValue = PARSER.getParsedMethod().getNumConditionals();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numConditionalsTest_shouldBeThree_2() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "threeIf_2";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 3;
+        int actualValue = PARSER.getParsedMethod().getNumConditionals();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void numConditionalsTest_shouldBeThree_3() {
+        String filePath = new File("src/test/java/parser/Stub.java").getAbsolutePath();
+        String methodName = "threeSwitchCase";
+        PARSER.parse(filePath, methodName);
+
+        int expectedValue = 3;
+        int actualValue = PARSER.getParsedMethod().getNumConditionals();
 
         assertEquals(expectedValue, actualValue);
     }
