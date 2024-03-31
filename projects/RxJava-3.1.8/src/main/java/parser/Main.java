c56d11cea5c24b3a9a5bafe152da6ac273bcd5b3
Sam Salek <33421876+sam-salek@users.noreply.github.com>
2024-03-31 17:44:35 +0200

    Updated Main

--- projects/RxJava-3.1.8/src/main/java/parser/Main.java
+++ projects/RxJava-3.1.8/src/main/java/parser/Main.java
@@ -3,18 +3,62 @@ package parser;
 public class Main {
 
     public static void main(String[] args) {
-        ParseAllBenchmarks parseAllBenchmarks = new ParseAllBenchmarks("C:\\Users\\super\\IntelliJ-projects\\MASTER\\benchmark-heuristics\\benchmarks\\results\\rxjava_RMAD.json");
-        parseAllBenchmarks.parseBenchmarks();
 
         MethodParser parser = new MethodParser(Integer.MAX_VALUE,
-                                    "projects\\RxJava-3.1.8\\src\\main\\java\\",
-                                     "projects\\RxJava-3.1.8\\src\\main\\java\\",
-                                      "rxjava");
+                "projects\\RxJava-3.1.8\\src\\main\\java\\",
+                "projects\\RxJava-3.1.8\\src\\main\\java\\",
+                "rxjava");
 
-        String filePath1 = "projects\\RxJava-3.1.8\\src\\test\\java\\io\\reactivex\\rxjava3\\internal\\schedulers\\InstantPeriodicTaskTest.java";
-        String methodName1 = "dispose3";
-        parser.parse(filePath1, methodName1);
+        boolean parseBenchmarks = true;
 
+        if (parseBenchmarks) {
+            BenchmarkParser parseAllBenchmarks = new BenchmarkParser("projects\\RxJava-3.1.8\\src\\test\\java\\",
+                    "benchmarks\\results\\rxjava_RMAD.json");
+
+            // parseAllBenchmarks.parseBenchmarks(parser, 0, 260, "benchmarks\\results\\parsedBenchmarks_test1.json");
+            // parseAllBenchmarks.parseBenchmarks(parser, 261, 520, "benchmarks\\results\\parsedBenchmarks_test2.json");
+            // parseAllBenchmarks.parseBenchmarks(parser, 521, 780, "benchmarks\\results\\parsedBenchmarks_test3.json");
+
+            parseAllBenchmarks.parseBenchmarks(parser, 10, 15, "benchmarks\\results\\parsedBenchmarks_test1.json");
+        }
+        else {
+
+            /*
+            // A lot of clas not found errors
+            String filePath1 = "projects\\RxJava-3.1.8\\src\\test\\java\\io\\reactivex\\rxjava3\\internal\\operators\\completable\\CompletableTimeoutTest.java";
+            String methodName1 = "timeoutException";
+            parser.parse(filePath1, methodName1);
+            */
+
+            String filePath1 = "projects\\RxJava-3.1.8\\src\\test\\java\\io\\reactivex\\rxjava3\\internal\\operators\\maybe\\MaybeConcatMapSingleTest.java";
+            String methodName1 = "flatMapSingleElementError";
+            parser.parse(filePath1, methodName1).print();
+
+            /*
+            String filePath2 = "projects\\RxJava-3.1.8\\src\\test\\java\\io\\reactivex\\rxjava3\\internal\\operators\\flowable\\FlowableOnBackpressureReduceWithTest.java";
+            String methodName2 = "exceptionFromSupplier";
+            parser.parse(filePath2, methodName2);
+
+            print(parser.parse(filePath2, methodName2));
+
+            String filePath3 = "projects\\RxJava-3.1.8\\src\\test\\java\\io\\reactivex\\rxjava3\\observable\\ObservableFuseableTest.java";
+            String methodName3 = "syncRange";
+            parser.parse(filePath3, methodName3);
+
+            print(parser.parse(filePath3, methodName3));
+
+             */
+        }
+
+         /*
+            String filePath1 = "projects\\RxJava-3.1.8\\src\\test\\java\\io\\reactivex\\rxjava3\\internal\\schedulers\\InstantPeriodicTaskTest.java";
+            String methodName1 = "dispose3";
+            parser.parse(filePath1, methodName1);
+
+            print(parser.parse(filePath1, methodName1));
+             */
+
+        /*
         String filePath2 = "projects\\RxJava-3.1.8\\src\\test\\java\\io\\reactivex\\rxjava3\\disposables\\DisposableTest.java";
         String methodName2 = "fromAction";
         parser.parse(filePath2, methodName2);
@@ -28,7 +72,7 @@ public class Main {
         String methodName3 = "mapFailWithSkip";
         MethodParser parser3 = new MethodParser(filePath3, methodName3);
         parser3.run();
-        */
+
 
         JsonCreator jsonCreator = new JsonCreator("E:\\Chalmers\\DATX05-MastersThesis\\test.json");
         //jsonCreator.add(parser1.toParsedMethod(),
@@ -38,28 +82,6 @@ public class Main {
 
 
         print(parser.parse(filePath1, methodName1));
-    }
-
-    /**
-     * Nicely prints the parsing of the provided ParsedMethod.
-     *
-     * @param parsedMethod The parsedMethod to print.
-     */
-    public static void print(ParsedMethod parsedMethod) {
-        System.out.println("\nMETHOD CALLS");
-        parsedMethod.getMethodCalls().forEach((k ,v) -> System.out.println(k + ": " + v));
-
-        System.out.println("\nOBJECT INSTANTIATIONS");
-        parsedMethod.getObjectInstantiations().forEach((k ,v) -> System.out.println(k + ": " + v));
-
-        System.out.println("\nPACKAGE ACCESSES");
-        parsedMethod.getPackageAccesses().forEach((k ,v) -> System.out.println(k + ": " + v));
-
-        System.out.println("\nSTATS");
-        System.out.println("Conditionals: " + parsedMethod.getNumConditionals());
-        System.out.println("Loops: " + parsedMethod.getNumLoops());
-        System.out.println("Nested loops: " + parsedMethod.getNumNestedLoops());
-        System.out.println("Method calls: " + parsedMethod.getNumMethodCalls());
-        System.out.println("Lines of code: " + parsedMethod.getLinesOfCode());
+        */
     }
 }
