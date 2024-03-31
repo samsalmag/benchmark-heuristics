032eab8056510c2111e6f45516eafd7a01eac6de
Sam Salek <33421876+sam-salek@users.noreply.github.com>
2024-03-31 17:39:42 +0200

    Cleaned up parsed method class and added new methods.

--- projects/RxJava-3.1.8/src/main/java/parser/ParsedMethod.java
+++ projects/RxJava-3.1.8/src/main/java/parser/ParsedMethod.java
@@ -10,8 +10,8 @@ import java.util.Map;
  */
 public class ParsedMethod {
 
-    private String filePath;
-    private String methodName;
+    private transient String filePath;
+    private transient String methodName;
 
     private Map<String, Integer> methodCalls = new HashMap<>();
     private Map<String, Integer> objectInstantiations = new HashMap<>();
@@ -21,132 +21,156 @@ public class ParsedMethod {
     private int numLoops;
     private int numNestedLoops;
     private int numMethodCalls;
-    private int linesOfCode; // counts physical lines of code
-    private int logicalLinesOfCode;
-    private int recursiveMethodCalls; // Non java lib calls
+    private int numRecursiveMethodCalls;    // Non java lib calls
+    private int linesOfCode;                // Counts physical lines of code
     private int linesOfCodeJunitTest;
-
-    public double getRMAD() {
-        return RMAD;
-    }
-
-    public void setRMAD(double RMAD) {
-        this.RMAD = RMAD;
-    }
-
-    private double RMAD; // stability value
-
-    public int getLogicalLinesOfCodeJunitTest() {
-        return logicalLinesOfCodeJunitTest;
-    }
-
-    public void setLogicalLinesOfCodeJunitTest(int logicalLinesOfCodeJunitTest) {
-        this.logicalLinesOfCodeJunitTest = logicalLinesOfCodeJunitTest;
-    }
-
+    private int logicalLinesOfCode;
     private int logicalLinesOfCodeJunitTest;
 
-    public int getLogicalLinesOfCode() {
-        return logicalLinesOfCode;
+
+    public void incrementNumConditionals(int incrementAmount) {
+        numConditionals += incrementAmount;
     }
 
-    public void setLogicalLinesOfCode(int logicalLinesOfCode) {
-        this.logicalLinesOfCode = logicalLinesOfCode;
+    public void incrementNumLoops(int incrementAmount) {
+        numLoops += incrementAmount;
     }
 
-    public int getRecursiveMethodCalls() {
-        return recursiveMethodCalls;
+    public void incrementNumNestedLoops(int incrementAmount) {
+        numNestedLoops += incrementAmount;
     }
 
-    public void setRecursiveMethodCalls(int recursiveMethodCalls_NonJavaLib) {
-        this.recursiveMethodCalls = recursiveMethodCalls_NonJavaLib;
+    public void incrementNumMethodCalls(int incrementAmount) {
+        numMethodCalls += incrementAmount;
     }
 
-    public int getLinesOfCodeJunitTest() {
-        return linesOfCodeJunitTest;
+    public void incrementNumRecursiveMethodCalls(int incrementAmount) {
+        numRecursiveMethodCalls += incrementAmount;
     }
 
-    public void setLinesOfCodeJunitTest(int linesOfCodeJunitTest) {
-        this.linesOfCodeJunitTest = linesOfCodeJunitTest;
+    public void incrementLinesOfCode(int incrementAmount) {
+        linesOfCode += incrementAmount;
     }
 
-    public String getFilePath() {
-        return filePath;
+    public void incrementLinesOfCodeJunitTest(int incrementAmount) {
+        linesOfCodeJunitTest += incrementAmount;
     }
+
+    public void incrementLogicalLinesOfCode(int incrementAmount) {
+        logicalLinesOfCode += incrementAmount;
+    }
+
+    public void incrementLogicalLinesOfCodeJunitTest(int incrementAmount) {
+        logicalLinesOfCodeJunitTest += incrementAmount;
+    }
+
+    /**
+     * Nicely prints the parsing data of this ParsedMethod.
+     *
+     */
+    public void print() {
+        System.out.println("\nMETHOD CALLS");
+        this.getMethodCalls().forEach((k ,v) -> System.out.println(k + ": " + v));
+
+        System.out.println("\nOBJECT INSTANTIATIONS");
+        this.getObjectInstantiations().forEach((k ,v) -> System.out.println(k + ": " + v));
+
+        System.out.println("\nPACKAGE ACCESSES");
+        this.getPackageAccesses().forEach((k ,v) -> System.out.println(k + ": " + v));
+
+        System.out.println("\nSTATS");
+        System.out.println("Conditionals: " + this.getNumConditionals());
+        System.out.println("Loops: " + this.getNumLoops());
+        System.out.println("Nested loops: " + this.getNumNestedLoops());
+        System.out.println("Number of method calls: " + this.getNumMethodCalls());
+        System.out.println("Number of recursive method calls: " + this.getNumRecursiveMethodCalls());
+        System.out.println("Lines of code: " + this.getLinesOfCode());
+        System.out.println("Lines of code (JUnit test): " + this.getLinesOfCodeJunitTest());
+        System.out.println("Logical lines of code: " + this.getLogicalLinesOfCode());
+        System.out.println("Logical lines of code (JUnit test): " + this.getLogicalLinesOfCodeJunitTest());
+    }
+
+    // --------------------------------------- //
+    // --------------- SETTERS --------------- //
+    // --------------------------------------- //
 
     public void setFilePath(String filePath) {
         this.filePath = filePath;
-    }
-
-    public String getMethodName() {
-        return methodName;
     }
 
     public void setMethodName(String methodName) {
         this.methodName = methodName;
     }
 
-    public Map<String, Integer> getMethodCalls() {
-        return methodCalls;
-    }
-
     public void setMethodCalls(Map<String, Integer> methodCalls) {
         this.methodCalls = methodCalls;
-    }
-
-    public Map<String, Integer> getObjectInstantiations() {
-        return objectInstantiations;
     }
 
     public void setObjectInstantiations(Map<String, Integer> objectInstantiations) {
         this.objectInstantiations = objectInstantiations;
     }
 
-    public Map<String, Integer> getPackageAccesses() {
-        return packageAccesses;
-    }
-
     public void setPackageAccesses(Map<String, Integer> packageAccesses) {
         this.packageAccesses = packageAccesses;
+    }
+
+    // --------------------------------------- //
+    // --------------- GETTERS --------------- //
+    // --------------------------------------- //
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public Map<String, Integer> getMethodCalls() {
+        return methodCalls;
+    }
+
+    public Map<String, Integer> getObjectInstantiations() {
+        return objectInstantiations;
+    }
+
+    public Map<String, Integer> getPackageAccesses() {
+        return packageAccesses;
     }
 
     public int getNumConditionals() {
         return numConditionals;
     }
 
-    public void setNumConditionals(int numConditionals) {
-        this.numConditionals = numConditionals;
-    }
-
     public int getNumLoops() {
         return numLoops;
-    }
-
-    public void setNumLoops(int numLoops) {
-        this.numLoops = numLoops;
     }
 
     public int getNumNestedLoops() {
         return numNestedLoops;
     }
 
-    public void setNumNestedLoops(int numNestedLoops) {
-        this.numNestedLoops = numNestedLoops;
-    }
-
     public int getNumMethodCalls() {
         return numMethodCalls;
-    }
-
-    public void setNumMethodCalls(int numMethodCalls) {
-        this.numMethodCalls = numMethodCalls;
     }
 
     public int getLinesOfCode() {
         return linesOfCode;
     }
 
-    public void setLinesOfCode(int linesOfCode) {
-        this.linesOfCode = linesOfCode;
+    public int getLinesOfCodeJunitTest() {
+        return linesOfCodeJunitTest;
+    }
+
+    public int getLogicalLinesOfCode() {
+        return logicalLinesOfCode;
+    }
+
+    public int getLogicalLinesOfCodeJunitTest() {
+        return logicalLinesOfCodeJunitTest;
+    }
+
+    public int getNumRecursiveMethodCalls() {
+        return numRecursiveMethodCalls;
     }
 }
