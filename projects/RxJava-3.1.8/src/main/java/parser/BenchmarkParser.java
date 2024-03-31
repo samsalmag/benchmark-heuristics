28bd443dc992bc8d43244d26e990a200236cfcd9
Sam Salek <33421876+sam-salek@users.noreply.github.com>
2024-03-31 17:39:07 +0200

    Cleaned up benchmark parser.

--- projects/RxJava-3.1.8/src/main/java/parser/BenchmarkParser.java
+++ projects/RxJava-3.1.8/src/main/java/parser/BenchmarkParser.java
@@ -1,8 +1,9 @@
 package parser;
 
-
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import parser.json.JsonCreator;
+import parser.json.JsonMethodItem;
 
 import java.io.File;
 import java.io.FileReader;
@@ -18,16 +19,16 @@ import java.nio.file.Paths;
  * Class that uses method and package name of the benchmarks in a benchmarkDict json file to
  * parse them (extracts stats, e.g. nested loops) and generates a new json file containing the results.
  * The structure of the read json file is a list of Pair<String, Double>, where String is the full
- * benchmark name, and Double is the RMAD value for the benchmark.
+ * benchmark name, and Double is the metric value for a property of the benchmark.
  *
  * @author Malte Åkvist
  */
-public class ParseAllBenchmarks {
+public class BenchmarkParser {
 
     private final String baseTestPath;
     private final List<SimpleEntry<String, Double>> benchmarkMap;
 
-    public ParseAllBenchmarks(String baseTestPath, String benchmarkJsonPath) {
+    public BenchmarkParser(String baseTestPath, String benchmarkJsonPath) {
         this.baseTestPath = baseTestPath;
         this.benchmarkMap = readJson(benchmarkJsonPath);
     }
@@ -39,28 +40,39 @@ public class ParseAllBenchmarks {
             }));
     }
 
-    public void parseBenchmarks(String outputPath) {
-        parseBenchmarks(0, benchmarkMap.size() - 1, outputPath);
+    public void parseBenchmarks(MethodParser parser, String outputPath) {
+        parseBenchmarks(parser, 0, benchmarkMap.size() - 1, outputPath);
     }
 
-    public void parseBenchmarks(int firstBenchmarkIndex, int lastBenchmarkIndex, String outputPath) {
+    /**
+     * Parses the unit tests that the benchmarks run.
+     *
+     * @param parser The parser to parse with.
+     * @param firstBenchmarkIndex Index of the first benchmark to parse
+     * @param lastBenchmarkIndex Index of the last benchmark to parse.
+     * @param outputPath Where to output the parsed benchmarks.
+     */
+    public void parseBenchmarks(MethodParser parser, int firstBenchmarkIndex, int lastBenchmarkIndex, String outputPath) {
         // Check so indexes are in range, correct them if they are not
         if (firstBenchmarkIndex < 0) firstBenchmarkIndex = 0;
         if (lastBenchmarkIndex > benchmarkMap.size() - 1) lastBenchmarkIndex = benchmarkMap.size() - 1;
-
+        if (lastBenchmarkIndex - firstBenchmarkIndex < 0) throw new IllegalArgumentException("Illegal index range.");
 
         JsonCreator jsonCreator = new JsonCreator(outputPath);
         setupShutdownHook(jsonCreator);
         int successfulIndex = 0;
         int iterationIndex = 0;
 
+        // Empty first line, for clean print.
+        System.out.println();
+
         try {
             for (int i = firstBenchmarkIndex; i <= lastBenchmarkIndex; i++) {
                 iterationIndex += 1;
 
                 String benchmark = benchmarkMap.get(i).getKey();
-                int last_Index = benchmark.lastIndexOf('_');
-                String method = benchmark.substring(last_Index + 1);
+                int lastIndex = benchmark.lastIndexOf('_');
+                String method = benchmark.substring(lastIndex + 1);
 
                 int secondLastDotIndex = benchmark.lastIndexOf('.', benchmark.lastIndexOf('.') - 1);
                 // Get the class path, each directory is currently separated by dots so have to use paths library to resolve path
@@ -69,30 +81,34 @@ public class ParseAllBenchmarks {
                 Path classPath = Paths.get("", parts);
 
                 Path base = Paths.get(baseTestPath);
-                Path resolvedPath = base.resolve(classPath); // concatenate base path with class path
-                Path benchmarkPath = Paths.get(resolvedPath + ".java"); // add .java to file extension
+                Path resolvedPath = base.resolve(classPath);                     // Concatenate base path with class path
+                Path benchmarkPath = Paths.get(resolvedPath + ".java");     // Add .java to file extension
+
                 System.out.println(benchmarkPath);
                 System.out.println(method);
-                MethodParser parser = new MethodParser(Integer.MAX_VALUE,
-                        "projects\\RxJava-3.1.8\\src\\main\\java\\",
-                        "projects\\RxJava-3.1.8\\src\\main\\java\\",
-                        "rxjava");
+                System.out.println("PARSING INDEX: " + (iterationIndex) + "/" + (lastBenchmarkIndex - firstBenchmarkIndex + 1));
 
-                ParsedMethod parsed = parser.parse(benchmarkPath.toString(), method);
-                System.out.println("INDEX, RUN METHODS: " + (i + 1) + "/" + benchmarkMap.size());
+                ParsedMethod parsedMethod = parser.parse(benchmarkPath.toString(), method);
+                JsonMethodItem jsonMethodItem = new JsonMethodItem(parsedMethod);
+
+                // Check for exception. Move to next benchmark if exception is thrown.
                 if (MethodParser.ambigousList.contains(benchmarkPath.toString()) ||
                         MethodParser.otherExceptionList.contains(benchmarkPath.toString()) ||
                         MethodParser.unsupportedOperationList.contains(benchmarkPath.toString()) ||
                         MethodParser.concurrentModificationList.contains(benchmarkPath.toString()) ||
                         MethodParser.noSuchElementList.contains(benchmarkPath.toString())) {
-                    System.out.println("INDEX, SUCCESSFUL RAN METHODS: " + successfulIndex + "/" + iterationIndex);
+                    System.out.println("ENCOUNTERED EXCEPTION! SKIPPING BENCHMARK. \n");
+                    System.out.println("SUCCESSFUL PARSINGS: " + successfulIndex + "/" + iterationIndex + "\n");
                     continue;
                 }
-                Double RMAD = benchmarkMap.get(i).getValue();
-                parsed.setRMAD(RMAD);
-                jsonCreator.add(parsed);
+
+                Double stabilityMetricValue = benchmarkMap.get(i).getValue();
+                jsonMethodItem.setStabilityMetricValue(stabilityMetricValue);
+                // jsonMethodItem.setCodeCoverageMetricValue(codeCoverageMetricValue);
+                jsonCreator.add(jsonMethodItem);
+
                 successfulIndex += 1;
-                System.out.println("INDEX, SUCCESSFUL RAN METHODS: " + successfulIndex + "/" + iterationIndex + "\n");
+                System.out.println("SUCCESSFUL PARSINGS: " + successfulIndex + "/" + iterationIndex + "\n");
             }
         }
         finally {
@@ -100,7 +116,7 @@ public class ParseAllBenchmarks {
             System.out.println("noSuchElementList list, size: " + MethodParser.noSuchElementList.size() + " list:" + MethodParser.noSuchElementList);
             System.out.println("unsupportedOperationList list, size: " + MethodParser.unsupportedOperationList.size() + " list:" + MethodParser.unsupportedOperationList);
             System.out.println("concurrentModificationList list, size: " + MethodParser.concurrentModificationList.size() + " list:" + MethodParser.concurrentModificationList);
-            System.out.println("Other list, size: " + MethodParser.otherExceptionList.size() + " list:" + MethodParser.otherExceptionList);
+            System.out.println("Other list, size: " + MethodParser.otherExceptionList.size() + " list:" + MethodParser.otherExceptionList + "\n");
 
             // Create json file even if there's an exception
             jsonCreator.createJson();
@@ -134,30 +150,4 @@ public class ParseAllBenchmarks {
         }
         return results;
     }
-
-    /*
-    // REMOVE THIS LATER, TAKEN FROM Main.java
-    public static void print(ParsedMethod parsedMethod) {
-        System.out.println("\nMETHOD CALLS");
-        parsedMethod.getMethodCalls().forEach((k ,v) -> System.out.println(k + ": " + v));
-
-        System.out.println("\nOBJECT INSTANTIATIONS");
-        parsedMethod.getObjectInstantiations().forEach((k ,v) -> System.out.println(k + ": " + v));
-
-        System.out.println("\nPACKAGE ACCESSES");
-        parsedMethod.getPackageAccesses().forEach((k ,v) -> System.out.println(k + ": " + v));
-
-        System.out.println("\nSTATS");
-        System.out.println("Conditionals: " + parsedMethod.getNumConditionals());
-        System.out.println("Loops: " + parsedMethod.getNumLoops());
-        System.out.println("Nested loops: " + parsedMethod.getNumNestedLoops());
-        System.out.println("Method calls: " + parsedMethod.getNumMethodCalls());
-        System.out.println("Recursive method calls: " + parsedMethod.getRecursiveMethodCalls());
-        System.out.println("Lines of code: " + parsedMethod.getLinesOfCode());
-        System.out.println("Logical lines of code: " + parsedMethod.getLogicalLinesOfCode());
-        System.out.println("Lines of code (junit test): " + parsedMethod.getLinesOfCodeJunitTest());
-        System.out.println("Logical lines of code (junit test): " + parsedMethod.getLogicalLinesOfCodeJunitTest());
-        System.out.println("RMAD: " + parsedMethod.getRMAD());
-    }
-    */
 }
