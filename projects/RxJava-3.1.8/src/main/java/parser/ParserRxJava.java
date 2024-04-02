99c4666fa87bb3ed8aa1654e7542b72baa8e853b
Sam Salek <33421876+sam-salek@users.noreply.github.com>
2024-04-03 01:59:03 +0200

    Fixed parser for RxJava

---
+++ projects/RxJava-3.1.8/src/main/java/parser/ParserRxJava.java
@@ -0,0 +1,23 @@
+package parser;
+
+import com.samsalek.benchmarkheuristics.parser.BenchmarkParser;
+import com.samsalek.benchmarkheuristics.parser.MethodParser;
+
+public class ParserRxJava {
+
+    public static void main(String[] args) {
+
+        MethodParser parser = new MethodParser(Integer.MAX_VALUE,
+                "projects\\RxJava-3.1.8\\src\\main\\java\\",
+                "projects\\RxJava-3.1.8\\src\\main\\java\\",
+                "rxjava");
+
+        BenchmarkParser parseAllBenchmarks = new BenchmarkParser("projects\\RxJava-3.1.8\\src\\test\\java\\",
+                                                            "benchmarks\\results\\rxjava_RMAD.json");
+
+        // RxJava has less than 1000 benchmarks
+        parseAllBenchmarks.parseBenchmarks(parser, 0, 249, "benchmarks\\results\\rxjava_parsedBenchmarks1.json");
+        parseAllBenchmarks.parseBenchmarks(parser, 250, 499, "benchmarks\\results\\rxjava_parsedBenchmarks2.json");
+        parseAllBenchmarks.parseBenchmarks(parser, 500, 1000, "benchmarks\\results\\rxjava_parsedBenchmarks3.json");
+    }
+}
