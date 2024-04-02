8392c6a4407ffb5e75817a140d969572f4d05848
Sam Salek <33421876+sam-salek@users.noreply.github.com>
2024-04-03 01:59:31 +0200

    Added parser for Stubby4j

---
+++ projects/stubby4j-7.6.0/src/main/java/parser/ParserStubby4j.java
@@ -0,0 +1,21 @@
+package parser;
+
+import com.samsalek.benchmarkheuristics.parser.BenchmarkParser;
+import com.samsalek.benchmarkheuristics.parser.MethodParser;
+
+public class ParserStubby4j {
+
+    public static void main(String[] args) {
+
+        MethodParser parser = new MethodParser(Integer.MAX_VALUE,
+            "projects\\stubby4j-7.6.0\\src\\main\\java\\",
+            "projects\\stubby4j-7.6.0\\src\\main\\java\\",
+            "stubby4j");
+
+        BenchmarkParser parseAllBenchmarks = new BenchmarkParser("projects\\stubby4j-7.6.0\\src\\test\\java\\",
+                                                            "");
+
+        // Stubby4j has less than 100 benchmarks
+        parseAllBenchmarks.parseBenchmarks(parser, 0, 99, "benchmarks\\results\\stubby4j_parsedBenchmarks1.json");
+    }
+}
