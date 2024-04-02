fa8beaf7dbdcf53ad37577488f3a763594890881
Sam Salek <33421876+sam-salek@users.noreply.github.com>
2024-04-03 01:59:22 +0200

    Added parser for Mockito

---
+++ projects/mockito-5.10.0/src/main/java/parser/ParserMockito.java
@@ -0,0 +1,22 @@
+package parser;
+
+import com.samsalek.benchmarkheuristics.parser.BenchmarkParser;
+import com.samsalek.benchmarkheuristics.parser.MethodParser;
+
+public class ParserMockito {
+
+    public static void main(String[] args) {
+
+        MethodParser parser = new MethodParser(Integer.MAX_VALUE,
+            "projects\\mockito-5.10.0\\src\\main\\java\\",
+            "projects\\mockito-5.10.0\\src\\main\\java\\",
+            "mockito");
+
+        BenchmarkParser parseAllBenchmarks = new BenchmarkParser("projects\\mockito-5.10.0\\src\\test\\java\\",
+                                                            "");
+
+        // Mockito has less than 500 benchmarks
+        parseAllBenchmarks.parseBenchmarks(parser, 0, 249, "benchmarks\\results\\mockito_parsedBenchmarks1.json");
+        parseAllBenchmarks.parseBenchmarks(parser, 250, 499, "benchmarks\\results\\mockito_parsedBenchmarks2.json");
+    }
+}
